c02c8b511cd16404c0669c519fb11512715022ed
Dawid Majewski <dawidmajewski1991@gmail.com>
2022-10-17 11:29:32 +0200

    object

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,18 +1,88 @@
-const defaultResult = 10;
-let currentResult = defaultResult;
-currentResult = (defaultResult + 10 * 2) / 4;
-console.log(defaultResult);
-console.log(currentResult);
-
-function add(num1, num2) {
-    const result = num1 + num2;
-    return result;
+// Get user entered number
+function getUserNumberInput() {
+    console.log(`Entered ${parseFloat(userInput.value)}.`);
+    return parseFloat(userInput.value);
 }
 
-const addResult = add(1, 8);
-console.log(addResult);
-console.log(add(2, 9));
-add(2, 5);
-let calculationDescription = `(${defaultResult} + 10 * 2)/4`;
+// Creates calculation description and making result output
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
 
-outputResult(addResult, calculationDescription);
+// Add user input number to last result
+function add() {
+    const enteredNumber = getUserNumberInput();
+    initialResult = currentResult;
+    currentResult += enteredNumber;
+    createAndWriteOutput("+", initialResult, enteredNumber);
+    const logEntry = {
+        operation: "ADD",
+        num1: initialResult,
+        num2: enteredNumber,
+        result: currentResult,
+    };
+    console.log(logEntry);
+    logEntries.push(enteredNumber);
+    console.log(logEntries);
+}
+
+// Add 1 to last result
+function add1() {
+    const enteredNumber = 1;
+    initialResult = currentResult;
+    currentResult ++;
+    createAndWriteOutput("+", initialResult, enteredNumber);
+    logEntries.push(enteredNumber);
+    console.log(logEntries);
+}
+
+// Subtract user input number from last result
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOutput("-", initialResult, enteredNumber);
+    logEntries.push(enteredNumber);
+    console.log(logEntries);
+}
+
+// Subtract 1 from last result
+function subtract1() {
+    const enteredNumber = 1;
+    initialResult = currentResult;
+    currentResult --;
+    createAndWriteOutput("-", initialResult, enteredNumber);
+    logEntries.push(enteredNumber);
+    console.log(logEntries);
+}
+// Multiply user input number by last result
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOutput("*", initialResult, enteredNumber);
+    logEntries.push(enteredNumber);
+    console.log(logEntries);
+}
+
+// Divide user input number by last result
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    initialResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteOutput("/", initialResult, enteredNumber);
+    logEntries.push(enteredNumber);
+    console.log(logEntries);
+}
+
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
+add1Btn.addEventListener("click", add1);
+subtract1Btn.addEventListener("click", subtract1);
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,8 +1,10 @@
 const userInput = document.getElementById("input-number");
 const addBtn = document.getElementById("btn-add");
 const subtractBtn = document.getElementById("btn-subtract");
+const subtract1Btn = document.getElementById("btn-subtract1");
 const multiplyBtn = document.getElementById("btn-multiply");
 const divideBtn = document.getElementById("btn-divide");
+const add1Btn = document.getElementById("btn-add1");
 
 const currentResultOutput = document.getElementById("current-result");
 const currentCalculationOutput = document.getElementById("current-calculation");
